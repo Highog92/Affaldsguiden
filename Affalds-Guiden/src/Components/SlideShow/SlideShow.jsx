ce92119277b88eb75ecc5de57d5f7f7d8ae41d45
Highog92 <Madsserritslev@gmail.com>
2024-03-04 12:18:39 +0100

    SlideShow virker

---
+++ Affalds-Guiden/src/Components/SlideShow/SlideShow.jsx
@@ -0,0 +1,23 @@
+import slideShowStyle from './SlideShowStyle.module.scss'
+import malerspande from '../../assets/Images/Slideshow/malerspande.jpg'
+import affaldSkov from '../../assets/Images/Slideshow/affald-skov-1.jpg'
+import affaldStrand from '../../assets/Images/Slideshow/affald-strand-2.jpg'
+import Carousel from 'react-material-ui-carousel'
+
+
+export function SlideShow() {
+
+
+    return (
+        <Carousel className={slideShowStyle.slideShow}
+            animation='slide'
+            indicators={false}
+            navButtonsAlwaysInvisible={false}
+            navButtonsAlwaysVisible={true}
+            height={1000}
+        >
+            <img src={affaldSkov} alt="affaldSkov" />
+            <img src={affaldStrand} alt="affaldStrand" />
+            <img src={malerspande} alt="malerspande" />
+        </Carousel>)
+}
--- Affalds-Guiden/src/Pages/Homepage.jsx
+++ Affalds-Guiden/src/Pages/Homepage.jsx
@@ -1,9 +1,40 @@
+import { SlideShow } from "../Components/SlideShow/SlideShow"
+import { Button } from "../Components/Button/Button"
+import HomepageStyle from './Styles/HomepageStyle.module.scss'
+
 export function Homepage() {
 
-    return (
-        <section>
-          
-  
-        </section>
-    )
+
+  return (
+    <section className={HomepageStyle.homePage}>
+      <SlideShow />
+
+      <div className={HomepageStyle.findStations}>
+        <div>
+          <p>Find og anmeld genbrugsstationer</p>
+          <div>
+
+            <Button
+              text="Find station"
+              bgColor="var(--darkGreen)"
+              sizeW={"120px"}
+              sizeH={"50px"}
+
+            />
+
+            <Button
+              text="Log ind"
+              bgColor="var(--darkGreen)"
+              sizeW={"120px"}
+              sizeH={"50px"}
+            />
+          </div>
+        </div>
+      </div>
+
+
+
+
+    </section>
+  )
 }
